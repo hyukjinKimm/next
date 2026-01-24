4139a5d5950b84ceef0bb1e803067da05f8ce7ae
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2026-01-24 16:38:41 +0900

    3

--- se02/my-app/src/pages/_app.tsx
+++ se02/my-app/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header>dd</header>
+      <header>글로벌헤더</header>
       <Component {...pageProps} />
     </>
   );
---
+++ se02/my-app/src/pages/search/index.tsx
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query;
+  console.log(q);
+  return <h1>Search{q}</h1>;
+}
